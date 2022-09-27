931ad29f4e48dff7dfd37ba3867c2f75bf555150
chxxing <choing26@naver.com>
2022-09-27 15:46:48 +0900

    220927

---
+++ src/pages/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainSlide from '../components/MainSlide';
+
+
+const Main = () => {
+    return (
+        <main>
+            <MainSlide />
+        </main>
+    )
+}
+
+export default Main
